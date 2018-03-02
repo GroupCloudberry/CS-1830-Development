ccc092df0fe51822b7b3602cfbda7fbd9a6fd356
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-02 15:42:15 +0000

    Preliminary PlayerData object for handling SQlite player table and directory cleanup

---
+++ src/PlayerData.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+class PlayerData:
+
+    def __init__(self):
+        self.connection = sqlite3.connect('playerdata.db')
+        sql_command = "CREATE TABLE IF NOT EXISTS players (" \
+                      "id integer PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, high_score INTEGER DEFAULT 0," \
+                      "currency INTEGER DEFAULT 0, lives INTEGER DEFAULT 3)"
+
+        self.connection.execute(sql_command)
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
+
+
