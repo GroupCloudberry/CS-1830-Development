9fd8941d1aa2f4a148efd8a6c47262ea931d5523
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-03 20:35:01 +0000

    Complete preliminary code for storage of player date using SQLite3

--- src/PlayerData.py
+++ src/PlayerData.py
@@ -1,12 +1,29 @@
 import sqlite3
+from enum import Enum
+
+
+class PlayerFields(Enum):
+    ID = "id"
+    PLAYER_NAME = "name"
+    LEVEL = "level"
+    HIGH_SCORE = "high_score"
+    CURRENCY = "currency"
+    LIVES = "lives"
+
+
+class IdNotFoundError(Exception):
+    pass
 
 
 class PlayerData:
 
+    TABLE = "players"
+
     def __init__(self):
         self.connection = sqlite3.connect('playerdata.db')
-        sql_command = "CREATE TABLE IF NOT EXISTS players (" \
-                      "id integer PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, high_score INTEGER DEFAULT 0," \
+        sql_command = "CREATE TABLE IF NOT EXISTS " + PlayerData.TABLE + " (" \
+                      "id integer PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, level INTEGER DEFAULT 1," \
+                      "high_score INTEGER DEFAULT 0," \
                       "currency INTEGER DEFAULT 0, lives INTEGER DEFAULT 3)"
         self.connection.execute(sql_command)
         self.connection.commit()
@@ -14,4 +31,38 @@ class PlayerData:
     def close(self):
         self.connection.close()
 
+    def id_exists(self, key):
+        sql_command = "SELECT id FROM " + PlayerData.TABLE + " WHERE id = " + key
+        cursor = self.connection.cursor()
+        cursor.execute(sql_command)
+        return cursor.fetchone() is not None
 
+    def add_player(self, player_name, level, score, currency, lives):
+        sql_command = "INSERT INTO " + PlayerData.TABLE + "[(name, level, high_score, currency, lives)]" \
+                      "VALUES (" + ", ".join(["'{}'".format(player_name), level, score, currency, lives]) + ");"
+        cursor = self.connection.cursor()
+        cursor.execute(sql_command)
+        self.connection.commit()
+        return cursor.lastrowid
+
+    def update_player(self, key, level, high_score, currency, lives):
+        if not self.id_exists(key):
+            raise IdNotFoundError
+        sql_command = "UPDATE " + self.TABLE + " SET level = " + level + ", high_score = " + high_score + \
+                      ", currency = " + currency + ", lives = " + lives + " WHERE id = " + key
+        self.connection.execute(sql_command)
+        self.connection.commit()
+
+    def retrieve(self, key, fields):
+        if not self.id_exists(key):
+            raise IdNotFoundError
+        sql_command = "SELECT " + fields + " from " + self.TABLE + " WHERE id = " + key
+        cursor = self.connection.cursor()
+        cursor.execute(sql_command)
+        return cursor.fetchone()
+
+    def get_field(self, key, field):
+        return self.retrieve(key, field)[0]
+
+    def get_all(self, key):
+        return self.retrieve(key, "*")
