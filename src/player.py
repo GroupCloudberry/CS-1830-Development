c6bd5fbe32c615f6de120d0b93a177ab6fc0ec16
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-04 15:57:58 +0000

    Basic object for Player sprite (only attributes implemented for now)

---
+++ src/player.py
@@ -0,0 +1,23 @@
+from player_attributes import PlayerAttributes
+
+
+class Player:
+
+    def __init__(self, attributes: PlayerAttributes=None):
+        self.name = None
+        self.score = 0
+        self.level = PlayerAttributes.DEFAULT_LEVEL
+        self.currency = PlayerAttributes.DEFAULT_CURRENCY
+        self.lives = PlayerAttributes.DEFAULT_LIVES
+        self.db_attributes = attributes
+
+    @staticmethod
+    def from_db(key):
+        player = Player()
+        attributes = PlayerAttributes.load(key)
+        player.name = attributes.name
+        player.level = attributes.level
+        player.currency = attributes.currency
+        player.lives = attributes.lives
+        return player
+
