1ba8bfecc911fd65dda3176e7c812192914e6f22
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-07 09:00:26 +0000

    Add reveal animation to game interface

--- src/game_interface.py
+++ src/game_interface.py
@@ -1,3 +1,5 @@
+from transition_clock import TransitionClock
+
 try:
     import simplegui
 except ImportError:
@@ -8,5 +10,27 @@ class GameInterface:
     def __init__(self, window):
         self.window = window
 
+        self.transition_clock = TransitionClock()
+        self.left_cover_x = 0
+        self.right_cover_x = self.window.__class__.WIDTH / 2
+
     def draw(self, canvas):
+        self.transition_clock.tick()
         canvas.draw_text("For actual game content.", (50, 150), 25, "White")
+        if self.left_cover_x > - self.window.__class__.WIDTH / 2:
+            self.reveal(canvas)
+
+    def reveal(self, canvas):
+        box_colour_left = "Orange"
+        box_colour_right = "Orange"
+        canvas.draw_polygon([(self.left_cover_x, 0), (self.left_cover_x, self.window.__class__.HEIGHT),
+                             (self.left_cover_x + self.window.__class__.WIDTH / 2, self.window.__class__.HEIGHT),
+                             (self.left_cover_x + self.window.__class__.WIDTH / 2, 0)],
+                            0, box_colour_left, box_colour_left)
+        canvas.draw_polygon([(self.right_cover_x, 0),
+                             (self.right_cover_x, self.window.__class__.HEIGHT),
+                             (self.right_cover_x + self.window.__class__.WIDTH / 2, self.window.__class__.HEIGHT),
+                             (self.right_cover_x + self.window.__class__.WIDTH / 2, 0)], 0,
+                            box_colour_right, box_colour_right)
+        self.left_cover_x -= 10
+        self.right_cover_x += 10
