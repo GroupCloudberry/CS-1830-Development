a8260f0640ac708ebfbc30a727718ba90ac5cb9a
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-08 13:43:42 +0000

    Change game transition colour to match background

--- src/game_interface.py
+++ src/game_interface.py
@@ -110,8 +110,8 @@ class GameInterface:
 
 
     def reveal(self, canvas):
-        box_colour_left = "Orange"
-        box_colour_right = "Orange"
+        box_colour_left = "Aqua"
+        box_colour_right = "Aqua"
         canvas.draw_polygon([(self.left_cover_x, 0), (self.left_cover_x, self.window.__class__.HEIGHT),
                              (self.left_cover_x + self.window.__class__.WIDTH / 2, self.window.__class__.HEIGHT),
                              (self.left_cover_x + self.window.__class__.WIDTH / 2, 0)],
