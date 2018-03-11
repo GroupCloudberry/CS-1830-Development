65227dc8063633ab45377ef1269286cb136be880
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-11 20:53:30 +0000

    Text reflow in story screen now fully functional

--- src/story_screen.py
+++ src/story_screen.py
@@ -5,12 +5,10 @@ except ImportError:
 
 
 class StoryPage:
-    def __init__(self):
+    def __init__(self, container1=None, container2=None):
         self.accent_colour = "Teal"
-        self.container1_img = None
-        self.container2_img = None
-        self.container1_text = ""
-        self.container2_text = ""
+        self.container1 = {"text": "", "image": None} if container1 is None else container1
+        self.container2 = {"text": "", "image": None} if container2 is None else container2
 
 
 class StoryScreenMenuItems:
@@ -24,29 +22,42 @@ class StoryScreen:
         self.box_reveal = 0.0
 
         # Testing variables to be removed after completion
-        self.testpage = StoryPage()
-        self.testpage.container1_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae odio sit amet metus rutrum accumsan. Aliquam vehicula arcu non arcu hendrerit lobortis. Curabitur eget ante ut est vulputate viverra. Aenean sed nisl rutrum, luctus magna sit amet, semper arcu. Nunc felis mi, cursus vel aliquam at, hendrerit ac libero. Nunc a consequat tortor, id elementum odio. Mauris tempor ipsum in dui iaculis, vel dapibus sem faucibus. In porta consectetur accumsan. Etiam dignissim turpis non nunc consectetur consequat."
-
+        self.test_page = StoryPage({"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                                           "Donec sed lorem in sem elementum pretium at ut eros. "
+                                           "Quisque sollicitudin arcu nulla, eu venenatis lorem tristique hendrerit. "
+                                           "Nam gravida tincidunt placerat. Sed sed nisl nec orci bibendum condimentum.",
+                                   "image": 1},
+                                   {"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                                            "Donec sed lorem in sem elementum pretium at ut eros. "
+                                            "Quisque sollicitudin arcu nulla, eu venenatis lorem tristique hendrerit. "
+                                            "Nam gravida tincidunt placerat. Sed sed nisl nec orci bibendum condimentum.",
+                                    "image": 1},
+                                   )
         self.page = 0
-        self.pages = [self.testpage]
-        self.image_box_width = 100
-        self.reflow_text()
+        self.pages = [self.test_page]
+        self.text = ["", ""]
+        self.images =[None, None]
 
-    def reflow_text(self):
+        self.text_size = 20
+        self.image_size = 150 # Value of 100 would be 100x100
+        self.reflow_content()
+
+    def reflow_content(self):
         # Algorithm to reflow overflowing lines of text
-        overflowing_words = 0
-        containers = [[[word for word in self.pages[self.page].container1_text.split()]],
-                      [[word for word in self.pages[self.page].container2_text.split()]]]
-        container_width = self.window.__class__.WIDTH - (75 * 2) - self.image_box_width
-        for container in containers:
-            for index, sentence in enumerate(container):
-                if self.window.frame.get_canvas_textwidth(" ".join(sentence), 25) > container_width:
+        # For each container on this page, split up the container text into words and put them into a list
+        containers = [[[word for word in self.pages[self.page].container1["text"].split()]],
+                      [[word for word in self.pages[self.page].container2["text"].split()]]]
+        container_width = [self.window.__class__.WIDTH - (75 * 2) - (0 if container["image"] is None else self.image_size)
+                           for container in [self.pages[self.page].container1, self.pages[self.page].container2]]
+        for containers_index, container in enumerate(containers):
+            # Iterate through each line in a container
+            for lines_index, line in enumerate(container):
+                if self.window.frame.get_canvas_textwidth(" ".join(line), self.text_size) > container_width[containers_index]:
+                    # Create a new line in that container if overflowing
                     container.append([])
-                while self.window.frame.get_canvas_textwidth(" ".join(sentence), 25) > container_width:
-                    container[index + 1].append(sentence.pop())
-                    overflowing_words += 1
-        print("Reflowed {} words into {} lines.".
-              format(str(overflowing_words), sum([len(container) for container in containers])))
+                while self.window.frame.get_canvas_textwidth(" ".join(line), self.text_size) > container_width[containers_index]:
+                    container[lines_index + 1].insert(0, line.pop())
+            self.text[containers_index] = container
 
     def draw_boxes(self, canvas):
         bg_colour = "Teal"
@@ -60,6 +71,14 @@ class StoryScreen:
                              (x + width, y + height),
                              (x + width, y)], 1, bg_colour, bg_colour)
         canvas.draw_text(text, (x + 15, y + 37), text_size, text_colour, "sans-serif")
+
+    def draw_text(self, canvas):
+        for index, container in enumerate([self.pages[self.page].container1, self.pages[self.page].container2]):
+            for line_number, line in enumerate(self.text[index]):
+                margin_left = self.image_size if (index + 1) % 2 == 0 else 0
+                container_spacing = 30 + ((self.text_size + 2) * len(self.text[index - 1])) if index > 0 else 0
+                canvas.draw_text(line, (75 + margin_left, 165 + ((self.text_size + 2) * line_number)
+                                        + container_spacing), self.text_size, "White")
 
     def draw_menu(self, canvas):
         canvas.draw_text(StoryScreenMenuItems.SKIP["label"], (self.window.__class__.WIDTH - 75 -
@@ -93,6 +112,7 @@ class StoryScreen:
     def draw_canvas(self, canvas):
         self.window.frame.set_keydown_handler(self.key_down)
         self.draw_boxes(canvas)
+        self.draw_text(canvas)
         self.draw_menu(canvas)
-        self.draw_page_number()
+        self.draw_page_number(canvas)
         self.reveal()
