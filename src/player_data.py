60d7f36438d6d20addc8783bd905005e9c6f2198
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-16 12:47:36 +0000

    Safer SQL insertion and querying with parameterised statements

--- src/player_data.py
+++ src/player_data.py
@@ -33,36 +33,33 @@ class PlayerData:
         self.connection.close()
 
     def id_exists(self, key):
-        sql_command = "SELECT id FROM " + PlayerData.TABLE + " WHERE id = " + str(key)
+        sql_command = "SELECT id FROM {} WHERE id=?".format(PlayerData.TABLE)
         cursor = self.connection.cursor()
-        cursor.execute(sql_command)
+        cursor.execute(sql_command, (key,))
         return cursor.fetchone() is not None
 
     def add_player(self, name, level, high_score, currency, lives):
-        sql_command = "INSERT INTO " + PlayerData.TABLE + " (name, level, high_score, currency, lives)" \
-                      " VALUES (" + ", ".join(["'{}'".format(name), str(level), str(high_score),
-                                              str(currency), str(lives)]) + ");"
+        sql_command = "INSERT INTO {} (name, level, high_score, currency, lives)".format(PlayerData.TABLE) +\
+                      " VALUES (?,?,?,?,?);"
         cursor = self.connection.cursor()
-        cursor.execute(sql_command)
+        cursor.execute(sql_command, (str(name), str(level), str(high_score), str(currency), str(lives)))
         self.connection.commit()
         return cursor.lastrowid
 
     def update_player(self, key, name, level, high_score, currency, lives):
         if not self.id_exists(key):
             raise IdNotFoundError
-        sql_command = "UPDATE " + self.TABLE + " SET name = " + "'{}'".format(name) + \
-                      ", level = " + str(level) + ", high_score = " + str(high_score) + \
-                      ", currency = " + str(currency) + ", lives = " + str(lives) + " WHERE id = " + str(key)
-        print(sql_command)
-        self.connection.execute(sql_command)
+        sql_command = "UPDATE {} SET name=?, level=?, high_score=?, currency=?, lives=? ".format(PlayerData.TABLE) +\
+                      "WHERE id =?"
+        self.connection.execute(sql_command, (str(name), str(level), str(high_score), str(currency), str(lives)))
         self.connection.commit()
 
     def retrieve_by_id(self, key, fields):
         if not self.id_exists(key):
             raise IdNotFoundError
-        sql_command = "SELECT " + fields.value + " from " + self.TABLE + " WHERE id = " + str(key)
+        sql_command = "SELECT {} from {} WHERE id=?".format(fields.value, PlayerData.TABLE)
         cursor = self.connection.cursor()
-        cursor.execute(sql_command)
+        cursor.execute(sql_command, (key,))
         return cursor.fetchone()
 
     def get_field_by_id(self, key, field):
@@ -70,22 +67,22 @@ class PlayerData:
 
     # noinspection PyTypeChecker
     def get_all_by_id(self, key):
-        return self.retrieve_by_id(key, "*")
+        return self.retrieve_by_id(str(key), "*")
 
     def get_ids_by_name(self, name):
-        sql_command = "SELECT id FROM " + PlayerData.TABLE + " WHERE name = " + name
+        sql_command = "SELECT id FROM {} WHERE name=?".format(PlayerData.TABLE)
         cursor = self.connection.cursor()
-        cursor.execute(sql_command)
+        cursor.execute(sql_command, (name,))
         return [row[0] for row in list(cursor)]
 
     def get_all_ids(self):
-        sql_command = "SELECT id from " + PlayerData.TABLE
+        sql_command = "SELECT id from {}".format(PlayerData.TABLE)
         cursor = self.connection.cursor()
         cursor.execute(sql_command)
         return [row[0] for row in list(cursor)]
 
     def delete_all(self):
-        sql_command = "DROP TABLE " + PlayerData.TABLE
+        sql_command = "DROP TABLE {}".format(PlayerData.TABLE)
         self.connection.execute(sql_command)
         self.connection.commit()
 
