5111ee084915188aab60e4418cde2f12df9f57dd
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-19 20:42:59 +0000

    Mouseclick to trigger pause menu, redraw transition upon resume

--- src/game_interface.py
+++ src/game_interface.py
@@ -68,6 +68,7 @@ class GameInterface:
         # Setting key up and down handlers and updating
         self.window.frame.set_keydown_handler(self.keydown)
         self.window.frame.set_keyup_handler(self.keyup)
+        self.window.frame.set_mouseclick_handler(self.mouse_down)
         self.updateKey()
 
         # Car
@@ -93,6 +94,10 @@ class GameInterface:
                             1, box_colour_right, box_colour_right)
         self.left_cover_x -= 25
         self.right_cover_x += 25
+
+    def mouse_down(self, position):
+        if 180 > position[0] > 75 and 25 > position[1] > 0:
+            self.hud.pause()
 
     def keyup(self,key):
         if key == simplegui.KEY_MAP['right']:
@@ -122,7 +127,12 @@ class GameInterface:
         elif key == simplegui.KEY_MAP['down']:
             self.mover.moveDown = True
         elif self.kb_compat.escape_key_pressed(key):
-            self.window.frame.set_draw_handler(self.window.pause_menu.draw_canvas)
+            self.pause()
+
+    def pause(self):
+        self.left_cover_x = 0
+        self.right_cover_x = self.window.__class__.WIDTH / 2
+        self.window.frame.set_draw_handler(self.window.pause_menu.draw_canvas)
 
     def checkRoadEnds(self):
         if self.mover.center.getX() < self.gameplay.endOfRoad_Origin.getX():
