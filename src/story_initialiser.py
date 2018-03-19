6d4806738e14bb6095939d6bf79737217fb6b1d1
John Su <john.su.2017@live.rhul.ac.uk>
2018-03-19 20:43:20 +0000

    Update story screen|

--- src/story_initialiser.py
+++ src/story_initialiser.py
@@ -11,23 +11,23 @@ class StoryInitialiser:
             {
                 "text": "Driver: Help the driver collect as many berries as possible so that "
                         "the alien will spare her another day.",
-                "image": "https://i.imgur.com/V0Loqpm.png"
+                "image": "https://i.imgur.com/tmWglsb.jpg"
             },
             {
-                "text": "Alien: Came from a far away planet, his only focus in life is eating berries; "
+                "text": "Alien: He comes from a planet far away and his only focus in life is eating berries; "
                         "he will spare only those who will provide him with enough berries for at least a day.",
-                "image": "https://i.imgur.com/V0tNdvW.png"
+                "image": "https://i.imgur.com/UUIBWmV.png"
             }
         ))
         self.pages.append(StoryPage(
             {
                 "text": "Each berry collected is worth 2 coins while the berry merchants "
                         "carry at at least 10 berries each.",
-                "image": "https://i.imgur.com/ecbmWex.png"
+                "image": "https://i.imgur.com/wmvmRww.jpg"
             },
             {
                 "text": "Be careful not to let the bear get too close. If the bear catches you, you lose one life!",
-                "image": "https://i.imgur.com/RhKrR0Z.png"
+                "image": "https://i.imgur.com/MYcG4Nm.jpg"
             }
         ))
 
